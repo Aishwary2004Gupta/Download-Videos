b55b83e137f01c34cd522584c8b884b88408241d
Aishwary Gupta <aish042004@gmail.com>
2024-10-02 11:55:50 +0530

    Update app.py

--- Video_Downloader/app.py
+++ Video_Downloader/app.py
@@ -8,7 +8,7 @@ import time
 app = Flask(__name__)
 app.secret_key = '1bd8a0bf5cde61924846417da9b121c2'
 
-progress_data = {"progress": 0}  # Global variable to store progress
+progress_data = {"progress": 0}  
 downloaded_file_path = None  # To store the path of the downloaded file
 
 # Function to get the default download path
