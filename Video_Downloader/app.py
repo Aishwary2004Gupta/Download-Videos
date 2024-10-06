4a9a3fb223581992f34bb104a18a5fc05cd0f572
Aishwary Gupta <aish042004@gmail.com>
2024-10-06 22:07:36 +0530

    Update app.py

--- Video_Downloader/app.py
+++ Video_Downloader/app.py
@@ -53,7 +53,6 @@ def download():
 
     temp_dir = tempfile.mkdtemp()
 
-    # Custom headers to simulate browser behavior
     custom_headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
