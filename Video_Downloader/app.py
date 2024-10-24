e7d02916462259379e8ee4ab6d158868c45e6715
Aishwary Gupta <aish042004@gmail.com>
2024-10-24 23:56:19 +0530

    Update app.py

--- Video_Downloader/app.py
+++ Video_Downloader/app.py
@@ -49,7 +49,7 @@ def download():
 
     print(f"Download requested for URL: {video_url} at {time.time()}")
 
-    # Use the Downloads folder 
+    # Use the 
     download_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
 
     custom_headers = {
